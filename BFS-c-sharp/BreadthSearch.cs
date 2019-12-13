9596bf27ac9d6414fb85cdd7831246f623b24eaa
András Rozner <roznernek@gmail.com>
2019-12-13 15:11:03 +0100

    friend os friends

---
+++ BFS-c-sharp/BreadthSearch.cs
@@ -0,0 +1,94 @@
+﻿using BFS_c_sharp.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BFS_c_sharp
+{
+
+    class BreadthSearch
+    {
+        private readonly List<UserNode> _users;
+        private Queue<UserNode> searchQueue = new Queue<UserNode>();
+        private List<UserNode> visited;
+        private List<UserNode> friendOfFriends;
+
+        public BreadthSearch(List<UserNode> users)
+        {
+            _users = users;
+        }
+
+        public int DistanceBetweenUsers(UserNode from, UserNode to)
+        {
+            visited = new List<UserNode>();
+
+            if (from.Equals(to))
+            {
+                return 0;
+            }
+
+            List<UserNode> temp = new List<UserNode>();
+            int distance = 0;
+            searchQueue.Enqueue(from);
+            while (searchQueue.Count > 0)
+            {
+                UserNode nextUser = searchQueue.Dequeue();
+                if (nextUser.Equals(to))
+                { 
+                    break;
+                }
+
+                if (visited.Contains(nextUser))
+                {
+                    continue;
+                }
+
+                visited.Add(nextUser);
+
+                foreach (var friend in nextUser.Friends)
+                {
+                    if (!temp.Contains(friend))
+                    {
+                        temp.Add(friend);
+                    }
+                }
+
+                if (searchQueue.Count == 0 && temp.Count > 0)
+                {
+                    distance++;
+                    foreach (var user in temp)
+                    {
+                        searchQueue.Enqueue(user);
+                    }
+                    temp.Clear();
+                }
+
+
+            }
+
+            return distance;
+        }
+
+        public List<UserNode> GetFriendsOfFriends(UserNode from, int distance)
+        {
+            friendOfFriends = new List<UserNode>();
+            FriendOfFriendsSearch(from, distance);
+            return friendOfFriends;
+        }
+
+        private void FriendOfFriendsSearch(UserNode from, int distance)
+        {
+            foreach (UserNode friend in from.Friends)
+            {
+                if (!friendOfFriends.Contains(friend))
+                {
+                    friendOfFriends.Add(friend);
+                }
+                if (distance > 0)
+                {
+                    FriendOfFriendsSearch(friend, distance - 1);
+                }
+                
+            }
+        }
+    }
+}
--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -8,14 +8,21 @@ namespace BFS_c_sharp
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
             RandomDataGenerator generator = new RandomDataGenerator();
             List<UserNode> users = generator.Generate();
+            BreadthSearch breadthSearch = new BreadthSearch(users);
 
             foreach (var user in users)
             {
                 Console.WriteLine(user);
             }
+            int distance = breadthSearch.DistanceBetweenUsers(users[0], users [1]);
+            Console.WriteLine("Distance between users: " + distance);
 
+            List<UserNode> friendsOfFriends = breadthSearch.GetFriendsOfFriends(users[0], 10);
+            string result = string.Join(", ", friendsOfFriends);
+            Console.WriteLine(result);
             Console.WriteLine("Done");
             Console.ReadKey();
         }
